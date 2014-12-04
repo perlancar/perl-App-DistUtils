2d0527a90531b481b035eb9bb631260d14a67e38
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2014-12-04 17:07:48 +0700

    Initial commit

---
+++ lib/App/DistUtils.pm
@@ -0,0 +1,23 @@
+package App::DistUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Collection of utilities related to Perl distributions
+
+=head1 SYNOPSIS
+
+This distribution provides several command-line utilities related to
+Perl distributions.
+
+
+=head1 SEE ALSO
+
+L<Dist::Util>
+
+=cut
