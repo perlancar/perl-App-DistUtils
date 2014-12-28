e7fdf833eb5175643104b756db0367b641014e99
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-28 18:10:40 +0700

    Replace App::PMUtils with depending on Complete::Module directly

--- lib/App/DistUtils.pm
+++ lib/App/DistUtils.pm
@@ -4,24 +4,6 @@ package App::DistUtils;
 # VERSION
 
 use 5.010001;
-use strict;
-use warnings;
-
-use Dist::Util ();
-
-our $_complete_dist = sub {
-    require Complete::Util;
-
-    my %args = @_;
-
-    my $word = $args{word} // '';
-    $word =~ s!/|::!-!g;
-    Complete::Util::complete_array_elem(
-        word=>$word,
-        array=>[Dist::Util::list_dists()],
-        ci=>1,
-    );
-};
 
 1;
 # ABSTRACT: Collection of utilities related to Perl distributions
