eb1ee794b7e99fc606d73f0e50fa87c562ff40ec
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-20 17:58:35 +0700

    Add script: dist-dir + refactor

--- lib/App/DistUtils.pm
+++ lib/App/DistUtils.pm
@@ -5,6 +5,33 @@ package App::DistUtils;
 
 use 5.010001;
 
+our %dist_arg_single = (
+    dist => {
+        schema => 'str*',
+        req => 1,
+        pos => 0,
+        completion => sub {
+            require Complete::Dist;
+            my %args = @_;
+            Complete::Dist::complete_dist(word=>$args{word});
+        },
+    },
+);
+
+our %dist_arg_multiple = (
+    dist => {
+        schema => ['array*', of=>'str*', min_len=>1],
+        req => 1,
+        pos => 0,
+        greedy => 1,
+        element_completion => sub {
+            require Complete::Dist;
+            my %args = @_;
+            Complete::Dist::complete_dist(word=>$args{word});
+        },
+    },
+);
+
 1;
 # ABSTRACT: Collection of utilities related to Perl distributions
 
